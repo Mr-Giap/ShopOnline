e0d57eec59c0825350af50fed7e1f48516dad587
VanSy Nguyen <synv09@outlook.com>
2021-07-24 17:49:09 +0700

    Commit code

--- ShopOnline/Startup.cs
+++ ShopOnline/Startup.cs
@@ -25,7 +25,7 @@ namespace ShopOnline
         }
         // Vòng đời của Dependency Injection: Transient, Singleton và Scoped
         public IConfiguration Configuration { get; }
-
+         
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
